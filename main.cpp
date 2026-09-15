5ff2584f8dddc75dedbdf84c344e4a8698b95a7b
Arsen <dulaev.arsen2006@gmail.com>
2026-09-15 15:41:52 +0300

    10 done

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
-
-void swap(int& a, int& b) {
-    int temp = a;
-    a = b;
-    b = temp;
-}
+#include <vector>
 
 int main() {
-    int a, b;
-    std::cin >> a >> b;
-    swap(a, b);
-    std::cout << a << " " << b;
+    std::vector<int> v;
+    int x;
+    while (std::cin >> x) {
+        v.push_back(x);
+    }
+    int max = v[0];
+    for (const int& i : v) {
+        if (i > max) {
+            max = i;
+        }
+    }
+    std::cout << max << std::endl;
     return 0;
 }
