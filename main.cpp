9950e83b3ec73e6c0d7fb93f175587f693ff9dc7
Arsen <dulaev.arsen2006@gmail.com>
2026-09-15 15:19:21 +0300

    07 done

--- main.cpp
+++ main.cpp
@@ -3,14 +3,10 @@
 int main() {
     int num;
     std::cin >> num;
-    if (num % 15 == 0) {
-        std::cout << "FizzBuzz" << std::endl;
-    } else if (num % 3 == 0) {
-        std::cout << "Fizz" << std::endl;
-    } else if (num % 5 == 0) {
-        std::cout << "Buzz" << std::endl;
-    } else {
-        std::cout << num << std::endl;
+    int sum = 0;
+    for (int i = 0; i <= num; i++) {
+        sum += i;
     }
+    std::cout << sum << std::endl;
     return 0;
 }
