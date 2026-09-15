ecf7fae248b6e7455947f63835821caf0188b7ab
Arsen <dulaev.arsen2006@gmail.com>
2026-09-15 15:13:01 +0300

    06 done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-#include <string>
 
 int main() {
-    std::string name, age;
-    std::cin >> name >> age;
-    std::cout << "Hi, " << name << "! You are " << age << " years old." << "\n";
+    int num;
+    std::cin >> num;
+    if (num % 15 == 0) {
+        std::cout << "FizzBuzz" << std::endl;
+    } else if (num % 3 == 0) {
+        std::cout << "Fizz" << std::endl;
+    } else if (num % 5 == 0) {
+        std::cout << "Buzz" << std::endl;
+    } else {
+        std::cout << num << std::endl;
+    }
     return 0;
 }
