82e003048db5785c01248afaf3d498f888c49a98
Arsen <dulaev.arsen2006@gmail.com>
2026-09-15 14:40:18 +0300

    03 done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main() {
-    int a, b;
-    std::cin >> a >> b;
-    std::cout << a + b << std::endl;
+    int w, h;
+    std::cin >> w >> h;
+    std::cout << w * h << std::endl;
     return 0;
 }
