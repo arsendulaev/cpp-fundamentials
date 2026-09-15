f81176fd9fed5134e8a355d53a9f577a6e78dc4c
Arsen <dulaev.arsen2006@gmail.com>
2026-09-15 15:34:07 +0300

    09 done

--- main.cpp
+++ main.cpp
@@ -1,13 +1,15 @@
 #include <iostream>
 
-int square(int n) {
-    return n * n;
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    n = square(n);
-    std::cout << n;
+    int a, b;
+    std::cin >> a >> b;
+    swap(a, b);
+    std::cout << a << " " << b;
     return 0;
 }
